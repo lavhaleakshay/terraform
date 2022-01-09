ac1641b480e8f81d920d45f5381d61c9474b3c18
Akshay Lavhale <lavhale.akshay8@gmail.com>
2022-01-09 23:38:41 +0530

    test

--- a/c/test2.tf
+++
@@ -1,5 +0,0 @@
-#tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-enable-bucket-logging
-resource "aws_s3_bucket" "my-bucket" {
-  bucket = "foobar"
-  acl    = "private"
-}
--- a/test.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_security_group_rule" "my-rule" {
-    type = "ingress"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
-}
